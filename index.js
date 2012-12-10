d4a57955ecab5228b2099cc4c9526995469b69fc
Corey Gaspard <cgaspard@gmail.com>
2012-12-10 11:01:11 -0600

    FF wants quotes around window.settimeout parameters

--- index.js
+++ index.js
@@ -152,9 +152,9 @@ function SelectProject() {
 	Kanban.BuildListGUI();
 
 	if(Mantis.DefaultFilterID !== null) {
-		window.setTimeout(LoadFilterAsync(Mantis.DefaultFilterID, 0, 0, DoneLoadingIssuesCallback), 100);
+		window.setTimeout("LoadFilterAsync(Mantis.DefaultFilterID, 0, 0, DoneLoadingIssuesCallback)", 100);
 		if(Mantis.ClosedIssuesFilterID !== null) {
-			window.setTimeout(LoadFilterAsync(Mantis.ClosedIssuesFilterID, 1, Kanban.NumberOfClosedMessagesToLoad, DoneLoadingIssuesCallback), 100);
+			window.setTimeout("LoadFilterAsync(Mantis.ClosedIssuesFilterID, 1, Kanban.NumberOfClosedMessagesToLoad, DoneLoadingIssuesCallback)", 100);
 		}
 	} else {
 		var retObj = Mantis.ProjectGetIssues(Mantis.CurrentProjectID, 0, 0);
