bf1854d5ed6845e16c47e0a29b8f17ef4152f17c
Corey Gaspard <corey.gaspard@mygait.net>
2013-05-14 07:29:38 -0500

    Added sample config file

---
+++ config-sample.js
@@ -0,0 +1,26 @@
+/// Point to the location of your server
+Mantis.ConnectURL = "http://bserver/api/soap/mantisconnect.php";
+
+
+/// The default filter to use when loading a projects issues
+/// If you leave this null, mantis will load whatever the last filter you used when you logged into the php site.
+Mantis.DefaultFilterID = "0";
+
+/// Use this value if you want to load additional closed issues, in addition to all the other statuses.   A good use for this is to load open issues 
+/// with DefaultFilterID, and load certain number of recently closed issues with these parameters.
+Mantis.ClosedIssuesFilterID = "0";
+
+/// How many issues should the call to the ClosedIssueFilterID return
+Kanban.NumberOfClosedMessagesToLoad = 10;
+
+/// This is the default project to be selected
+Mantis.CurrentProjectSelection = "0";
+
+/// This is used to just define the Default Settings object, this info gets saved to local storage for next login
+var DefaultSettings = {
+	username:"",
+	stayLoggedIn:1,
+	lastAccessTime:0,
+	currentProject:Mantis.CurrentProjectSelection
+};
+
