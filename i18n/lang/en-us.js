365f375914515a55186bd58da22e71c5e2380769
Zipher <zipher04@yahoo.com.tw>
2015-12-06 21:05:43 +0800

    Add localization support by JavaScript.

---
+++ i18n/lang/en-us.js
@@ -0,0 +1,88 @@
+var langObj = {
+	"textAddNewStory":"Add a New Story",
+"textAddNote":"Add Note",
+"textAddTag":"Add",
+"textAssignedTo":"Assigned To:",
+"textAttachments":"Attachments",
+"textCategory":"Category",
+"textChooseFile":"Choose File To Add...",
+"textClose":"Close",
+"textDateSubmitted":"Date Submitted:",
+"textDelete":"Delete",
+"textDeleteAttachemets":"Clear Upload List",
+"textDescription":"Description",
+"textExistTags":"Add Existing",
+"textFilter":"Select Filter",
+"textGeneral":"General",
+"textHandler":"Handler",
+"textHide":"Hide",
+"textHigh":"High",
+"textHistory":"History",
+"textImmediate":"Immediate",
+"textLogout":"Logout",
+"textLow":"Low",
+"textNew":"Add Story",
+"textNo":"No",
+"textNormal":"Normal",
+"textNotes":"Notes",
+"textPiorities":"Piorities",
+"textPriority":"Priority",
+"textProject":"Project",
+"textReload":"Reload Kanban",
+"textReporter":"Reporter",
+"textSaveChanges":"Save changes",
+"textSaveNewStory":"Save New Story",
+"textSelectProject":"Select Project",
+"textSetting":"Settings",
+"textStatus":"Status",
+"textSteps":"Steps",
+"textSummary":"Summary",
+"textTag1":"Tag 1",
+"textTag2":"Tag 2",
+"textTags":"Tags (click to delete):",
+"textUploadAttachments":"Upload Attachments To Story",
+"textUrgent":"Urgent"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
